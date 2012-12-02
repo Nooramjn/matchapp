d2d0bf12598f37f4330868973ac214cde43eeaf7
Nooramjn <nmslmani@gmail.com>
2012-12-02 18:59:49 +0300

    Added partial header and footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,17 @@
 class PagesController < ApplicationController
   def home
+  @title = "Home"
   end
 
-  def about
+  def about_us
+  @title = "About"
   end
 
-  def contact
+  def contact_us
+  @title = "Contact"
   end
 
   def privacy_policy
+  @title = "Privacy Policy"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def title
+	base_title = "Manchester United FC"
+	if @title.nil?
+		base_title
+	else
+		"Manchester United FC | #{@title}"
+end
+end
 end
