cba742cbcbd7deda5f2dc0bf4547b2cb13940937
Hawra Abdulla <hmabdull@qatar.cmu.edu>
2012-12-02 19:10:47 +0300

    validations done

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,13 @@
 class Match < ActiveRecord::Base
+
+
+	attr_accessible :type :home :opponent_id
+	has_many :tickets
+	belong_to :opponenets
+	
+	validates :type, :presence =>true 
+	validates :home , :presence =>true 
+	validates :opponent_id, :presence =>true 
+
+
 end
