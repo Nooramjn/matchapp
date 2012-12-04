fa5d4b9d850ba834f41ccc1529e660134545c90d
Nooramjn <nmslmani@gmail.com>
2012-12-05 00:31:17 +0300

    added password attribute

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -1,6 +1,7 @@
 class Fan < ActiveRecord::Base
 
-attr_accessible :name, :email
+attr_accessible :name, :email, :password, :password_confirmation
+attr_accessor :password, :password_confirmation
 
 has_many :tickets
 
